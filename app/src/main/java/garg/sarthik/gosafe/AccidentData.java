b12534fe2b8d71ea89e9f4ba3b46517a3a912b89
Sarthik <sarthikgarg01@gmail.com>
2018-10-28 01:09:01 +0530

    Added Accident Data HARDCODED

---
+++ app/src/main/java/garg/sarthik/gosafe/AccidentData.java
@@ -0,0 +1,16 @@
+package garg.sarthik.gosafe;
+
+public class AccidentData {
+
+    String state;
+    String city;
+    String Locality;
+    int percentage;
+
+    public AccidentData(String state, String city, String locality, int percentage) {
+        this.state = state;
+        this.city = city;
+        Locality = locality;
+        this.percentage = percentage;
+    }
+}
--- app/src/main/java/garg/sarthik/gosafe/MainActivity.java
+++ app/src/main/java/garg/sarthik/gosafe/MainActivity.java
@@ -1,8 +1,19 @@
 package garg.sarthik.gosafe;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +23,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +39,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,7 +48,31 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        ArrayList<AccidentData> accidentDataArrayList = new ArrayList<>();
+        accidentDataArrayList.add(new AccidentData("Delhi","Delhi","Dilshad Garden",20));
+        accidentDataArrayList.add(new AccidentData("Delhi","Delhi","Connaught Place",23));
+        accidentDataArrayList.add(new AccidentData("Delhi","Delhi","Rithala",25));
+        accidentDataArrayList.add(new AccidentData("Delhi","Delhi","Chandni Chowk",55));
+        accidentDataArrayList.add(new AccidentData("Uttar Pradesh","Ghaziabad","Indrapuram",30));
+        accidentDataArrayList.add(new AccidentData("Uttar Pradesh","Ghaziabad","Vaishali",20));
+        accidentDataArrayList.add(new AccidentData("Uttar Pradesh","Ghaziabad","Mohan Nagar",51));
+
+
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+
+        String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+        String city = addresses.get(0).getLocality();
+        String state = addresses.get(0).getAdminArea();
+        String country = addresses.get(0).getCountryName();
+        String postalCode = addresses.get(0).getPostalCode();
+        String knownName = addresses.get(0).getFeatureName();
     }
+
 
     @Override
     public void onBackPressed() {
@@ -50,13 +82,6 @@ public class MainActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
 
     @Override
